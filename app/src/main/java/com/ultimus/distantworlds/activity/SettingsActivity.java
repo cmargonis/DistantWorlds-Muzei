de6f63ccb8e932a9cc5f5b4d19f0b7db604027f3
Chris Margonis <chris.margonis@gmail.com>
2018-11-03 15:03:48 +0200

    Add copyright headers

--- app/src/main/java/com/ultimus/distantworlds/activity/SettingsActivity.java
+++ app/src/main/java/com/ultimus/distantworlds/activity/SettingsActivity.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2018 Chris Margonis
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.ultimus.distantworlds.activity;
 
 
@@ -9,10 +25,10 @@ import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceScreen;
-import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import com.ultimus.distantworlds_muzei.R;
