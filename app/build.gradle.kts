69b692a767ad623db6bc7a35edee6a9bc3ba5c23
Chris Margonis <chris.margonis@gmail.com>
2022-09-24 08:43:05 +0300

    Update compile and taretSdk to 33 (Android 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,11 +42,11 @@ fun getImgurApiProperties(): Properties {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.ultimus.distantworlds_muzei"
         minSdk = 21
-        targetSdk = 30
+        targetSdk = 33
         versionCode = 10
         versionName = "3.4.1"
 
